3d0a06d0bfb6f536ff88fc5602c72adaa017524d
Derek Dinsmore <derek@dinsmoredesign.com>
2021-10-14 15:57:22 -0700

    Updating packages

--- components/AwardsContainer.vue
+++ components/AwardsContainer.vue
@@ -1,37 +1,47 @@
 <template>
-
     <div class="awards">
         <div class="container">
             <div class="row">
                 <div class="col-md-4 col-xs-12">
                     <div class="row middle-xs award">
-                        <div class="col-xs-4 icon-container" v-html="require('!svg-inline-loader!../static/images/the-knot.svg')"></div>
+                        <div
+                            class="col-xs-4 icon-container"
+                            v-html="
+                                require('!svg-inline-loader!../static/images/the-knot.svg')
+                            "
+                        ></div>
                         <div class="col-xs-8 content">
                             <p>
                                 <small>2017, 2018, 2019 &</small>
                             </p>
-                            <h3>
-                                <strong>2020</strong> the knot
-                            </h3>
+                            <h3><strong>2020</strong> the knot</h3>
                             <p>Best of Weddings</p>
                             <p>+ <strong>2020</strong> Hall of Fame</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4 col-xs-12">
-                    <div class="row middle-xs icon-container award" v-html="require('!svg-inline-loader!../static/images/style-me-pretty.svg')"></div>
+                    <div
+                        class="row middle-xs icon-container award"
+                        v-html="
+                            require('!svg-inline-loader!../static/images/style-me-pretty.svg')
+                        "
+                    ></div>
                 </div>
                 <div class="col-md-4 col-xs-12">
                     <div class="row middle-xs award">
-                        <div class="col-xs-4 icon-container" v-html="require('!svg-inline-loader!../static/images/wedding-wire.svg')"></div>
+                        <div
+                            class="col-xs-4 icon-container"
+                            v-html="
+                                require('!svg-inline-loader!../static/images/wedding-wire.svg')
+                            "
+                        ></div>
                         <div class="col-xs-8 content">
                             <p>
                                 <small>2017, 2018 &</small>
                             </p>
-                            <h3>
-                                <strong>2019</strong> WeddingWire
-                            </h3>
-                            <p> Couples' Choice Awards</p>
+                            <h3><strong>2019</strong> WeddingWire</h3>
+                            <p>Couples' Choice Awards</p>
                         </div>
                     </div>
                 </div>
@@ -39,108 +49,88 @@
             <div class="row single-icon">
                 <div class="today">
                     <h3>As seen on</h3>
-                    <div v-html="require('!svg-inline-loader!../static/images/today.svg')"></div>
+                    <div
+                        v-html="
+                            require('!svg-inline-loader!../static/images/today.svg')
+                        "
+                    ></div>
                 </div>
             </div>
         </div>
     </div>
-
 </template>
 
-
-
- 
-
 <script>
-
-    export default {
-
-        
-
-    }
-
+export default {};
 </script>
 
-
-
-
-
 <style lang="scss" scoped>
+.awards {
+    background-color: #5e5e5e;
+    color: #fff;
 
-    .awards {
-        background-color: #5e5e5e;
-        color: #FFF;
-
-        .container {
-            width: 90%;
-        }
-
-        .award {
-            padding: 3rem 0;
-        }
-
-        .single-icon {
-            justify-content: center;
-            text-align: center;
-
-            .today {
-                padding: 3rem 0;
-                max-width: 300px;
-
-                h3 {
-                    text-align: right;
-                    font-size: 1.54rem;
-                }
-
-                /deep/ svg {
-                    width: 100%;
-                }
-
-            }
-
-        }
-
-        .icon-container {
-            justify-content: center;
-            text-align: center;
-
-            /deep/ svg {
-                height: 125px;
-
-                * {
-                    fill: #FFF;
-                }
-
-            }
-
-            @media screen and (min-width: 1350px) {
-
-                /deep/ svg {
-                    height: 150px;
-                }
-
-            }
-
-        }
-
-        p,
-        h3 {
-            margin: 0;
-        }
-
-        h3 {
-            font-size: 2.2rem;
-            font-weight: 200;
-        }
-        p {
-            font-size: 1.5rem;
-            font-weight: 400;
-        }
-
-        .small {
-            font-size: 100%;
-        }
-
+    .container {
+        width: 90%;
     }
 
+    .award {
+        padding: 3rem 0;
+    }
+
+    .single-icon {
+        justify-content: center;
+        text-align: center;
+
+        .today {
+            padding: 3rem 0;
+            max-width: 300px;
+
+            h3 {
+                text-align: right;
+                font-size: 1.54rem;
+            }
+
+            ::v-deep svg {
+                width: 100%;
+            }
+        }
+    }
+
+    .icon-container {
+        justify-content: center;
+        text-align: center;
+
+        ::v-deep svg {
+            height: 125px;
+
+            * {
+                fill: #fff;
+            }
+        }
+
+        @media screen and (min-width: 1350px) {
+            ::v-deep svg {
+                height: 150px;
+            }
+        }
+    }
+
+    p,
+    h3 {
+        margin: 0;
+    }
+
+    h3 {
+        font-size: 2.2rem;
+        font-weight: 200;
+    }
+    p {
+        font-size: 1.5rem;
+        font-weight: 400;
+    }
+
+    .small {
+        font-size: 100%;
+    }
+}
 </style>
--- components/Hero.vue
+++ components/Hero.vue
@@ -1,101 +1,86 @@
 <template>
-
     <section class="hero" :style="{ 'min-height': height }">
-        <slot/>
-        <a role="button" href="javascript:void(0);" title="Continue to content" @click="scrollToContent" v-if="showScrollIcon">
+        <slot />
+        <a
+            role="button"
+            href="javascript:void(0);"
+            title="Continue to content"
+            @click="scrollToContent"
+            v-if="showScrollIcon"
+        >
             <i class="icon-angle-down subtitle is-1" aria-hidden="true"></i>
         </a>
         <footer v-if="$slots.footer">
-            <slot name="footer"/>
+            <slot name="footer" />
         </footer>
     </section>
-
 </template>
 
-
-
-
-
 <script>
-
-    export default {
-
-        props: {
-            background: {
-                type: String,
-                required: false
-            },
-            height: {
-                type: String,
-                required: false,
-                default: '100vh'
-            },
-            showScrollIcon: {
-                type: Boolean,
-                required: false,
-                default: false
-            },
-            scrollAnchor: {
-                type: String,
-                required: false,
-                default: 'content'
-            }
+export default {
+    props: {
+        background: {
+            type: String,
+            required: false
         },
-        methods: {
-            scrollToContent() {
-
-                document.getElementById(this.scrollAnchor).scrollIntoView({
-                    behavior: 'smooth'
-                });
-
-            }
+        height: {
+            type: String,
+            required: false,
+            default: "100vh"
+        },
+        showScrollIcon: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        scrollAnchor: {
+            type: String,
+            required: false,
+            default: "content"
         }
-
+    },
+    methods: {
+        scrollToContent() {
+            document.getElementById(this.scrollAnchor).scrollIntoView({
+                behavior: "smooth"
+            });
+        }
     }
-
+};
 </script>
 
-
-
-
-
 <style lang="scss" scoped>
+.hero {
+    -webkit-background-size: cover !important;
+    -moz-background-size: cover !important;
+    -o-background-size: cover !important;
+    background-size: cover !important;
+    color: white;
+    width: 100%;
 
-    .hero {
-            -webkit-background-size: cover !important;
-            -moz-background-size: cover !important;
-            -o-background-size: cover !important;
-        background-size: cover !important;
-        color: white;
-        width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+}
 
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
+footer {
+    margin-top: 4rem;
+    text-align: center;
+
+    ::v-deep * {
         text-align: center;
     }
+}
 
+@media screen and (min-height: 550px) {
     footer {
-        margin-top: 4rem;
-        text-align: center;
-
-        /deep/ * {
-            text-align: center;
-        }
-
+        position: absolute;
+        height: 5rem;
+        display: flex;
+        align-items: center;
+        bottom: 0;
+        width: 100%;
     }
-
-    @media screen and (min-height: 550px) {
-
-        footer {
-            position: absolute;
-            height: 5rem;
-            display: flex;
-            align-items: center;
-            bottom: 0;
-            width: 100%;
-        }
-
-    }
-
+}
 </style>
